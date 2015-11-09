b7add526466bb21ac8f60d1571c00e7892c01f20
Paul <paul.giuliano@gmail.com>
2015-11-08 20:29:03 -0800

    Deployed site Sun Nov  8 20:29:03 PST 2015

--- deploy.sh
+++ deploy.sh
@@ -2,6 +2,7 @@
 
 echo -e "\033[0;32mDeploying updates to GitHub...\033[0m"
 
+echo -e "\033[0;32mBuilding and committing Public...\033[0m"
 # Build the project.
 hugo # if using a theme, replace by `hugo -t <yourtheme>`
 
@@ -10,18 +11,34 @@ cp CNAME public/
 # Go To Public folder
 cd public
 
-# Add changes to git.
+# Add changes to public
 git add -A
 
-# Commit changes.
+# Commit changes to public
 msg="rebuilding site `date`"
 if [ $# -eq 1 ]
   then msg="$1"
 fi
 git commit -m "$msg"
 
-# Push source and build repos.
+# Push source and build repos in public
 git push origin master
 
-# Come Back
+# Come Back to master
 cd ..
+
+echo -e "\033[0;32mCommitting and pushing Master...\033[0m"
+# Add changes to master
+git add -A
+
+# Commit changes to master
+msg="Deployed site `date`"
+if [ $# -eq 1 ]
+  then msg="$1"
+fi
+git commit -m "$msg"
+
+# Push source and build master
+git push origin master
+
+echo -e "\033[0;32mDone!\033[0m"
